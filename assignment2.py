ab2555b49d341072e093d17ab4a231c539fb20c8
yordi-7-cyber <tyordi09@gmail.com>
2025-03-19 12:06:42 +0300

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -1,24 +1,6 @@
-students = {"yordanos": 21, "tinsae": 20, "feven": 22,"Lulit":20}
-def HUCISA_Students(action, name=None, age=None):
-    if action == "display":
-        print("\n".join(f"{n}: {a}" for n, a in students.items()))
-    elif action == "add" and name and age:
-        students[name] = age
-        print(f"{name} added.")
-    elif action == "update" and name and age:
-        if name in students: 
-            students[name] = age
-            print(f"{name}'s age updated.")
-        else:
-            print(f"{name} not found.")
-    elif action == "remove" and name:
-        if name in students:
-            del students[name]
-            print(f"{name} removed.")
-        else:
-            print(f"{name} not found.")
-HUCISA_Students("display")
-HUCISA_Students("add", "miko", 21)
-HUCISA_Students("update", "surafel", 23)
-HUCISA_Students("remove", "hananita")
-HUCISA_Students("display")
+def unpack_and_print(t):
+    a, b, c = t
+    print(a, b, c)
+t = (10, 20, 30)
+unpack_and_print(t)
+
