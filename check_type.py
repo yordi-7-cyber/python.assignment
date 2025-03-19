8ae5955da2b61b2923bbe151ccfe23963076a9bb
yordi-7-cyber <tyordi09@gmail.com>
2025-03-19 13:32:14 +0300

    Add files via upload

---
+++ check_type.py
@@ -0,0 +1,11 @@
+def check_type(variable):
+    print(f"The type of the variable is: {type(variable)}")
+x = 10            
+y = 3.14
+z = "HUCISA"        
+a = True
+
+check_type(x)
+check_type(y)
+check_type(z)
+check_type(a)
