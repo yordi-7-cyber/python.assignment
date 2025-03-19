4dd2f51c7ecd97173678ff2489a602c7af05f12f
yordi-7-cyber <tyordi09@gmail.com>
2025-03-19 11:48:05 +0300

    Add files via upload

--- assignment.py
+++ assignment.py
@@ -1,11 +1,18 @@
-def check_type(variable):
-    print(f"The type of the variable is: {type(variable)}")
-x = 10            
-y = 3.14
-z = "HUCISA"        
-a = True
+def list1(n):
+    return n
+num=input("Enter your number:")
+n=list(map(int,num.split()))
+result=list1(n)
+print("current list is:",result)
+add=int(input("enter a value to add to the list:"))
+result.append(add)
+print("List after adding a number:",result)
+remove=int(input("Enter a value to remove from the list:"))
+if remove in result:
+    result.remove(remove)
+    print("After removing the value the list will be:", result)
+else:
+    print("Not found.")
 
-check_type(x)
-check_type(y)
-check_type(z)
-check_type(a)
+if result:
+    print("maximum value:",max(result),"\nMinimum value:",min(result))
